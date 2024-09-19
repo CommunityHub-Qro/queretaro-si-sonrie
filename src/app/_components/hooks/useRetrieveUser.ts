ebaced45274bb75cce37edfc126cb4df180d9010
SALV <a01707122@tec.mx>
2024-09-19 12:49:02 -0600

    [ADD] sign in and log in Server side functionality

--- src/app/_components/hooks/useRetrieveUser.ts
+++ src/app/_components/hooks/useRetrieveUser.ts
@@ -1,6 +1,45 @@
+import { newUser } from './../../../server/api/routers/newUser';
+'use server'
+
+import { error } from "console";
 import { api } from "~/trpc/server";
+const argon2 = require('argon2')
 
+type newUser = {
+  name: string,
+  password: string, 
+  email: string,
+}
 
-export const useGetSession = async (user: string, password: string) => {
+export const useCreateUser = async (newUser: newUser) => {
+  const hash = await argon2.hash(newUser.password)
+    .then((h: string) => api.newUser.create({
+      name: newUser.name,
+      email: newUser.email,
+      password: h,
+    }))
+    .catch((error: any) => console.log(error));
+  
+  
+}
+
+export const useRetrieveUser = async (name: string, password: string) => {
+  const user = await api.newUser.getUser({name})
+    .then((u) => u);
+  if (!user) {
+    return "Error: user not found";
+  }
+    
+  try {
+    if (await argon2.verify(user.password, password)) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+  catch (error) {
+    return error
+  }
   
 }
--- src/app/access-page/page.tsx
+++ src/app/access-page/page.tsx
@@ -1,44 +1,96 @@
 'use client'
 
-import {  useSession } from "next-auth/react";
 import { useState } from "react";
+import { useCreateUser, useRetrieveUser } from "../_components/hooks/useRetrieveUser";
 
 export default function AccessPage(){
-    const session = useSession();
-    // console.log(session);
-    const [user, setUser] = useState<string>();
-    const [password, setPassword] = useState<string>();
+    const [user, setUser] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [signIn, setSignIn] = useState(false);
 
-    function login(e: any) {
+    let userSession;
+
+    function sessionLogIn(e: any) {
         e.preventDefault();
-        useRetrieveUser
+        const fetch = async () => {
+            userSession = await useRetrieveUser(user, password)
+                                .then((value) => value)
+                                .catch((error) => 
+                                    console.log("Error" + error)
+                                );
+            console.log(userSession)
+        }
+        fetch();
     }
-    function recuperarContraseña() {
 
+    function sessionSignIn(e: any) {
+        if (!signIn){
+            e.preventDefault();
+            setSignIn(true);
+            return
+        }
+        if (user === '') {
+            const userEntry = document.getElementById('user');
+            userEntry?.classList.add('placeholder-red-500')
+            userEntry?.setAttribute('placeholder', 'Usuario: NECESARIO')
+        }
+        if (email === '') {
+            const emailEntry = document.getElementById('email');
+            emailEntry?.classList.add('placeholder-red-500')
+            emailEntry?.setAttribute('placeholder', 'Email: NECESARIO')
+        }
+        if (password === '') {
+            const passwordEntry = document.getElementById('password');
+            passwordEntry?.classList.add('placeholder-red-500')
+            passwordEntry?.setAttribute('placeholder', 'Contraseña: NECESARIO')
+        }
+        if (password === '' || email === '' || user === '') {
+            return 
+        }
+
+        useCreateUser({
+            name: user,
+            email: email,
+            password: password,
+        });
+        alert('Usuario creado')
+        window.location.reload();
+        
     }
+
+    function recuperarContraseña() {
+        console.log("recuperar contraseña")
+        const fetch = async () => {
+            const userR = await useRetrieveUser(user, password)
+                .then((userR) => console.log(user))
+                .catch((error) => console.log("ERROR! " + error));
+        }
+    }
+    
     return(
-        <div className="h-[90vh] flex">
+        <div className="md:h-[90vh] md:flex max-md:h-full">
             <img src="images/Voluntariado2.png" className="h-[90vh]"/>
-            <form className="flex justify-center items-center flex-col h-full w-full gap-10" id="sing_in">
-                <input className="w-3/5 input p-2" placeholder="Usuario" type="text" onChange={(u) => setUser(u.target.value)}/>
-                <input className="w-3/5 input p-2" placeholder="Contraseña" type="password" onChange={(p) => setPassword(p.target.value)}/>
-                {/* <button className="w-1/3 h-12 border-2 border-gray-300 rounded-lg flex items-center justify-center"
-                    onClick={() => signIn()}>
-                    <img src="icons/google.webp" className="size-10"/>
-                    <p>Ingresar con google</p>
-                </button> */}
-                <button type="submit" className="w-32 drop-shadow-md bg-third rounded-full text-center hover:bg-[rgb(255,40,40)] py-2 h-[3rem] items-center text-white font-bold text-xl"
-                    onClick={(e) => login(e)}>
-                    Entrar
+            <aside className="flex flex-col justify-center items-center w-full max-md:mt-10">
+                <form className="flex justify-center items-center flex-col h-1/2 w-full gap-10" id="sing_in">
+                    <input className="w-3/5 input p-2" placeholder={`${signIn ? 'User' : 'Email'}`} type="text" onChange={(u) => setUser(u.target.value)} id='user'/>
+                    {signIn ? 
+                        <input className="w-3/5 input p-2" placeholder="Email" type="text" onChange={(u) => setEmail(u.target.value)} id='email'/> : null}
+                    <input className="w-3/5 input p-2" placeholder="Contraseña" type="password" onChange={(p) => setPassword(p.target.value)} id='password'/>
+                    <button type="submit" className={`w-32 drop-shadow-md bg-third rounded-full text-center hover:bg-[rgb(255,40,40)] py-2 h-[3rem] items-center text-white font-bold text-xl ${signIn ? 'hidden' : null}`} 
+                        onClick={(e) => sessionLogIn(e)}>
+                        Log in
+                    </button>
+                    <a className={`text-[rgb(255,65,65)] text-sm -mt-8 cursor-pointer ${signIn ? 'hidden' : null} `} onClick={() => recuperarContraseña()}>
+                        ¿Olvidaste tu contraseña?
+                    </a>
+                </form>
+                <hr className={`w-11/12 border-b-[1] border-black m-5 ${signIn ? 'hidden' : ''}`}/>
+                <button type="submit" className="w-32 drop-shadow-md border-third border-4 rounded-full text-center hover:bg-third py-2 h-[3rem] items-center text-black font-bold text-xl hover:text-white flex justify-center"
+                    onClick={(e) => sessionSignIn(e)}>
+                    Sign in
                 </button>
-                <a className="text-[rgb(255,65,65)] text-sm -mt-8" onClick={() => recuperarContraseña()}>
-                    ¿Olvidaste tu contraseña?
-                </a>
-            </form>
+            </aside>
         </div>
     );
-}
-
-function useRetrieveUser() {
-    throw new Error("Function not implemented.");
 }

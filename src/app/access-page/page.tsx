4a28547b9753112d1ba7457f627774b55e5ea533
SALV <a01707122@tec.mx>
2024-09-23 12:24:31 -0600

    [ADD] send email to retrieve password

--- src/app/access-page/page.tsx
+++ src/app/access-page/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useCreateUser, useRetrieveUser } from "../_components/hooks/useRetrieveUser";
+import RecuperarContrasenia from "../_components/organisms/RecuperarContrasenia";
 
 export default function AccessPage(){
     const [user, setUser] = useState<string>('');
@@ -9,6 +10,7 @@ export default function AccessPage(){
     const [password, setPassword] = useState<string>('');
     const [signIn, setSignIn] = useState(false);
     const [emailValidation, setEmailValidation] = useState(false)
+    const [recuperarContraseniaState, setRecuperarContraseniaState] = useState(false)
 
     let userSession;
 
@@ -77,47 +79,52 @@ export default function AccessPage(){
             return 
         }        
 
-        useCreateUser({ // Generar usuario con valores de entrada
+    // Generar usuario con valores de entrada
+        useCreateUser({ 
             name: user,
             email: email,
             password: password,
         });
         alert('Usuario creado')
         window.location.reload();
-        
     }
 
     function recuperarContraseña() {
-        console.log("recuperar contraseña")
+        setRecuperarContraseniaState(true);
     }
-    
-    return(
-        <div className="md:h-[90vh] md:flex max-md:h-full">
-            <img src="images/Voluntariado2.png" className="h-[90vh] -mt-2"/>
-            <aside className="flex flex-col justify-center items-center w-full max-md:mt-10">
-                <form className="flex justify-center items-center flex-col h-1/2 w-full gap-10" id="sing_in">
-                    <input className="w-3/5 input p-2" placeholder={`${signIn ? 'User' : 'Email'}`} type="text" onChange={(u) => setUser(u.target.value)} id='user'/>
-                    {signIn ? 
-                    <>
-                        <input className="w-3/5 input p-2" placeholder="Email" type="text" onChange={(u) => setEmail(u.target.value)} id='email'/> 
-                        <p className={`text-[rgb(255,65,65)] text-sm -m-8 ${emailValidation ? '' : 'hidden'}`}>Error: email no valido</p>
-                    </>
-                        : null}
-                    <input className="w-3/5 input p-2" placeholder="Contraseña" type="password" onChange={(p) => setPassword(p.target.value)} id='password'/>
-                    <button type="submit" className={`w-32 drop-shadow-md bg-third rounded-full text-center hover:bg-[rgb(255,40,40)] py-2 h-[3rem] items-center text-white font-bold text-xl ${signIn ? 'hidden' : null}`} 
-                        onClick={(e) => sessionLogIn(e)}>
-                        Log in
+    if (recuperarContraseniaState)
+    {
+        return (<RecuperarContrasenia/>)
+    } else {
+        return(
+            <div className="md:h-[90vh] md:flex max-md:h-full">
+                <img src="images/Voluntariado2.png" className="h-[90vh] -mt-2"/>
+                <aside className="flex flex-col justify-center items-center w-full max-md:mt-10">
+                    <form className="flex justify-center items-center flex-col h-1/2 w-full gap-10" id="sing_in">
+                        <input className="w-3/5 input p-2" placeholder={`${signIn ? 'User' : 'Email'}`} type="text" onChange={(u) => setUser(u.target.value)} id='user'/>
+                        {signIn ? 
+                        <>
+                            <input className="w-3/5 input p-2" placeholder="Email" type="text" onChange={(u) => setEmail(u.target.value)} id='email'/> 
+                            <p className={`text-[rgb(255,65,65)] text-sm -m-8 ${emailValidation ? '' : 'hidden'}`}>Error: email no valido</p>
+                        </>
+                            : null}
+                        <input className="w-3/5 input p-2" placeholder="Contraseña" type="password" onChange={(p) => setPassword(p.target.value)} id='password'/>
+                        <button type="submit" className={`w-32 drop-shadow-md bg-third rounded-full text-center hover:bg-[rgb(255,40,40)] py-2 h-[3rem] items-center text-white font-bold text-xl ${signIn ? 'hidden' : null}`} 
+                            onClick={(e) => sessionLogIn(e)}>
+                            Log in
+                        </button>
+                        <a className={`text-[rgb(255,65,65)] text-sm -mt-8 cursor-pointer ${signIn ? 'hidden' : null} `} onClick={() => recuperarContraseña()}>
+                            ¿Olvidaste tu contraseña?
+                        </a>
+                    </form>
+                    <hr className={`w-11/12 border-b-[1] border-black m-5 ${signIn ? 'hidden' : ''}`}/>
+                    <button type="submit" className="w-32 drop-shadow-md border-third border-4 rounded-full text-center hover:bg-third py-2 h-[3rem] items-center text-black font-bold text-xl hover:text-white flex justify-center"
+                        onClick={(e) => sessionSignIn(e)}>
+                        Sign in
                     </button>
-                    <a className={`text-[rgb(255,65,65)] text-sm -mt-8 cursor-pointer ${signIn ? 'hidden' : null} `} onClick={() => recuperarContraseña()}>
-                        ¿Olvidaste tu contraseña?
-                    </a>
-                </form>
-                <hr className={`w-11/12 border-b-[1] border-black m-5 ${signIn ? 'hidden' : ''}`}/>
-                <button type="submit" className="w-32 drop-shadow-md border-third border-4 rounded-full text-center hover:bg-third py-2 h-[3rem] items-center text-black font-bold text-xl hover:text-white flex justify-center"
-                    onClick={(e) => sessionSignIn(e)}>
-                    Sign in
-                </button>
-            </aside>
-        </div>
-    );
+                </aside>
+            </div>
+        );
+
+    }
 }

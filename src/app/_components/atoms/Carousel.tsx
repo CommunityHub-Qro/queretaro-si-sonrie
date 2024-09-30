26c115497a86912c6d6a5509d39646015a82101c
Frida Arcadia <frida.arcadialv@gmail.com>
2024-09-30 12:33:37 -0600

    [DELETE] arrows from carousel

--- src/app/_components/atoms/Carousel.tsx
+++ src/app/_components/atoms/Carousel.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useState, useEffect } from 'react';
-import { ChevronLeft, ChevronRight } from 'react-feather';
 
 interface CarouselProps {
   images: string[]; 
@@ -10,18 +9,12 @@ export const Carousel: React.FC<CarouselProps> = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [autoPlay, setAutoPlay] = useState(true);
 
-  const handlePrev = () => {
-    setCurrentIndex((currentIndex - 1 + images.length) % images.length);
-  };
-
-  const handleNext = () => {
-    setCurrentIndex((currentIndex + 1) % images.length);
-  };
-
   useEffect(() => {
     let intervalId;
     if (autoPlay) {
-      intervalId = setInterval(handleNext, 2500); // change the image every 3 seconds
+      intervalId = setInterval(() => {
+        setCurrentIndex((currentIndex + 1) % images.length);
+      }, 2500); // change the image every 3 seconds
     }
     return () => {
       clearInterval(intervalId);
@@ -43,17 +36,6 @@ export const Carousel: React.FC<CarouselProps> = ({ images }) => {
       onMouseLeave={handleMouseLeave}
     >
       <img src={images[currentIndex]} alt="Current image" />
-      <div className='absolute inset-0 flex items-center justify-between p-4'>
-        <button onClick={handlePrev} className='p-1 rounded-full shadow text-gray-900 hover:bg-white bg-slate-200'>
-            <ChevronLeft size={20}/>
-        </button>
-        <button onClick={() => {
-          handleNext();
-          setAutoPlay(false);
-        }} className='p-1 rounded-full shadow text-gray-900 hover:bg-white bg-slate-200'>
-            <ChevronRight size={20}/>
-        </button>
-      </div>
       <div className='absolute bottom-4 right-0 left-0'>
         <div className='flex items-center justify-center gap-2'>
             {images.map((_, i) => (

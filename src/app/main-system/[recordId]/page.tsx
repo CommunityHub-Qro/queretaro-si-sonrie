e9adfb1142bbe308338acab34152121195f432f0
EdgarRetes <edgar.retes24@gmail.com>
2024-10-11 09:26:40 -0600

    [ADD] back button

--- src/app/main-system/[recordId]/page.tsx
+++ src/app/main-system/[recordId]/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import { getRecords } from "../../_components/hooks/getRecords";
-import UpdatePatientRecord from '../../_components/organisms/updatePatientRecord'; 
+import UpdatePatientRecord from "../../_components/organisms/updatePatientRecord";
+import Link from "next/link";
 
 interface Patient {
   id: string;
@@ -19,7 +20,7 @@ const RecordDetails = () => {
   const [records, setRecords] = useState<Patient[]>([]);
   const [patient, setPatient] = useState<Patient | null>(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [isEditing, setIsEditing] = useState(false); 
+  const [isEditing, setIsEditing] = useState(false);
 
   const { data, error, isLoading: isFetching } = getRecords();
 
@@ -38,7 +39,7 @@ const RecordDetails = () => {
 
   const handleUpdateSuccess = (updatedData: Patient) => {
     setPatient(updatedData);
-    setIsEditing(false); 
+    setIsEditing(false);
   };
 
   if (isLoading) {
@@ -50,28 +51,52 @@ const RecordDetails = () => {
   }
 
   return (
-    <div className="max-w-xl mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Detalles del Registro</h1>
+    <div className="mx-auto max-w-xl p-4">
+      <Link
+        href={"/main-system"}
+        className="flex h-full w-32 items-center justify-center rounded-full bg-third py-2 text-center text-white drop-shadow-md hover:bg-[rgb(255,40,40)]"
+      >
+        Regresar
+      </Link>
+      <h1 className="mb-4 text-2xl font-bold">Detalles del Registro</h1>
       {isEditing ? (
         <UpdatePatientRecord
-        patientId={patient.id}
-        initialData={{
-          name: patient.name,
-          birth_date: patient.birth_date ? patient.birth_date.toISOString().split('T')[0] : '',
-          register_date: patient.register_date ? patient.register_date.toISOString().split('T')[0] : '',
-          dx: patient.dx,
-          notes: patient.notes,
-        }}
-        onSuccess={handleUpdateSuccess}
-      />
+          patientId={patient.id}
+          initialData={{
+            name: patient.name,
+            birth_date: patient.birth_date
+              ? patient.birth_date.toISOString().split("T")[0]
+              : "",
+            register_date: patient.register_date
+              ? patient.register_date.toISOString().split("T")[0]
+              : "",
+            dx: patient.dx,
+            notes: patient.notes,
+          }}
+          onSuccess={handleUpdateSuccess}
+        />
       ) : (
         <div className="space-y-4">
-          <p><strong>ID del registro:</strong> {recordId}</p>
-          <p><strong>Nombre:</strong> {patient.name}</p>
-          <p><strong>Fecha de nacimiento:</strong> {patient.birth_date.toDateString()}</p>
-          <p><strong>Diagnóstico:</strong> {patient.dx}</p>
-          <p><strong>Notas:</strong> {patient.notes}</p>
-          <button onClick={handleEditClick} className="bg-yellow-500 text-white py-2 px-4 rounded hover:bg-yellow-600">
+          <p>
+            <strong>ID del registro:</strong> {recordId}
+          </p>
+          <p>
+            <strong>Nombre:</strong> {patient.name}
+          </p>
+          <p>
+            <strong>Fecha de nacimiento:</strong>{" "}
+            {patient.birth_date.toDateString()}
+          </p>
+          <p>
+            <strong>Diagnóstico:</strong> {patient.dx}
+          </p>
+          <p>
+            <strong>Notas:</strong> {patient.notes}
+          </p>
+          <button
+            onClick={handleEditClick}
+            className="rounded bg-yellow-500 px-4 py-2 text-white hover:bg-yellow-600"
+          >
             Editar
           </button>
         </div>

5878a13649a57f2f4a91d92361abc6f467edd541
SALV <a01707122@tec.mx>
2024-10-29 19:23:17 -0600

    [DELETE] organigrama

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,12 +114,6 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Organigrama */}
-      <div className="text-bold poppins -mt-48 flex h-screen flex-col items-center p-5 text-6xl">
-        <div className="flex h-full items-center">
-          <img src={"images/organigrama.png"} className="h-full w-full drop-shadow-md" />
-        </div>
-      </div>
       {/* Servicios */}
       <Decorations
         color1="fifth"

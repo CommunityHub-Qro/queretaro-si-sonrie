f9bf74ca07e1c16ff0452213a41ece77284350f6
kamimartinez <kamiii.martineeez@gmail.com>
2024-11-17 17:17:22 -0600

    [FIX] testimonios

--- src/app/impact-page/Impact.tsx
+++ src/app/impact-page/Impact.tsx
@@ -2,33 +2,44 @@
 
 import React, { useState } from "react";
 import Link from "next/link";
-import { Decorations } from "../_components/atoms/Decorations";
-import { ChevronLeft, ChevronRight, Star } from "lucide-react";
-import { useEffect, useRef } from 'react';
-import Chart from 'chart.js/auto';
-import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { Decorations, Icon } from "../_components/atoms/Decorations";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useEffect, useRef } from "react";
+import { HydrateClient } from "~/trpc/server";
+import Chart, { ChartType, ChartData, ChartOptions } from "chart.js/auto";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
 const testimonials = [
   {
     id: 1,
-    text: "Lorem ipsum dolor sit amet consectetur. Amet pharetra vitae porttitor diam. Odio consectetur aliquam lobortis risus donec molestie efficiend. Lobortis convallis id mauris amet orci id ultricies. Nibh vitae mi in habitant nisi amet quam. Etiam consectetur faucibus elit venenatis congue odio ac. Id commodo mi erat adipiscing ac proin a vitae ut.",
-    author: "Madre de Benny",
-    rating: 4,
+    text: "Dios les bendiga a todos por tan hermoso labor.",
+    author: "Angel Ortiz Licea",
   },
   {
     id: 2,
-    text: "Lorem ipsum dolor sit amet consectetur. Amet pharetra vitae porttitor diam. Odio consectetur aliquam lobortis risus donec molestie efficiend.",
-    author: "Madre de Jenny",
-    rating: 5,
+    text: "Doctora, a usted y a todo el equipo de IQUEC, nuestro mas sicnero y honesto agradecimiento eterno por la invaluable labor que han hecho por todos nuestros pequeños.",
   },
   {
     id: 3,
-    text: "Lorem ipsum dolor sit amet consectetur. Amet pharetra vitae porttitor diam. Odio consectetur aliquam lobortis risus donec molestie efficiend.",
-    author: "Padre de Tommy",
-    rating: 4,
+    text: "Mil gracias por todo lo que hacen por nuestros niños Dios los bendiga.",
+    author: "Luis Angel Garcia",
+  },
+  {
+    id: 4,
+    text: "Gracias a usted y todos los doctores y equipo que tienen a hecho el labor con nuestros pequeños. Dios los bendiga.",
+    author: "Jose Jonathan Ordaz",
+  },
+  {
+    id: 5,
+    text: "Mil gracias por todo su trabajo, apoyo y seguimiento para nuestros niños. Es un equipo muy bonito y competente, mi niña va feliz a sus consultas y gracias a Dios va muy bien. Cuenten con nuestro apoyo.",
+    author: "Patty Rodriguez",
+  },
+  {
+    id: 6,
+    text: "Son unos angeles de verdad.",
+    author: "Ramon Peralta",
   },
 ];
-
 
 const Impact = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -49,77 +60,78 @@ const Impact = () => {
   };
 
   const chartData = [200, 500, 300, 450, 800, 500];
-  const labels = ['2019', '2020', '2021', '2022', '2023', '2024'];
+  const labels = ["2019", "2020", "2021", "2022", "2023", "2024"];
 
-  // Set a fixed height for the chart
-  const fixedHeight = 150; // Fixed height of 150 pixels
-
-  // Calculate the maximum value in chartData to scale heights
+  const fixedHeight = 150;
   const maxValue = Math.max(...chartData);
-
-  // Reference for Doughnut Chart canvas and Chart instance
   const doughnutChartRef = useRef<HTMLCanvasElement | null>(null);
   const chartInstanceRef = useRef<Chart<"doughnut"> | null>(null);
 
   useEffect(() => {
-    const ctx = doughnutChartRef.current?.getContext('2d');
+    const ctx = doughnutChartRef.current?.getContext("2d");
 
-    // If a chart instance already exists, destroy it before creating a new one
     if (chartInstanceRef.current) {
       chartInstanceRef.current.destroy();
     }
 
     if (ctx) {
-      // Register the ChartDataLabels plugin
       Chart.register(ChartDataLabels);
 
-      // Create a new doughnut chart and store the instance in chartInstanceRef
       chartInstanceRef.current = new Chart<"doughnut", number[], unknown>(ctx, {
-        type: 'doughnut',
+        type: "doughnut",
         data: {
-          labels: ['55 (0.4%)', '44 (16.3%)', '39 (14.4%)', '27 (10.0%)', '24 (8.9%)', '24 (8.9%)'],
-          datasets: [{
-            label: 'Procedimientos',
-            data: [0.4, 16.3, 14.4, 10.0, 8.9, 8.9], // Data is explicitly of type number[]
-            backgroundColor: [
-              '#007bff', // Blue
-              '#ff5733', // Red
-              '#ffc300', // Yellow
-              '#28a745', // Green
-              '#fd9644', // Orange
-              '#8e44ad', // Purple
-            ],
-            borderWidth: 5,
-            borderColor: '#ffffff',
-            hoverOffset: 4,
-            borderRadius: 10, // Rounded corners for the arcs
-          }]
+          labels: [
+            "55 (0.4%)",
+            "44 (16.3%)",
+            "39 (14.4%)",
+            "27 (10.0%)",
+            "24 (8.9%)",
+            "24 (8.9%)",
+          ],
+          datasets: [
+            {
+              label: "Procedimientos",
+              data: [0.4, 16.3, 14.4, 10.0, 8.9, 8.9],
+              backgroundColor: [
+                "#007bff",
+                "#ff5733",
+                "#ffc300",
+                "#28a745",
+                "#fd9644",
+                "#8e44ad",
+              ],
+              borderWidth: 5,
+              borderColor: "#ffffff",
+              hoverOffset: 4,
+              borderRadius: 10,
+            },
+          ],
         },
         options: {
           responsive: true,
-          cutout: '60%', // Controls the doughnut thickness
+          cutout: "60%",
           plugins: {
             legend: {
-              display: false // Hide the default legend
+              display: false,
             },
             tooltip: {
-              enabled: false // Disable tooltips
+              enabled: false,
             },
-            // Data labels plugin to show numbers
             datalabels: {
-              color: '#000', // Set the text color
+              color: "#000",
               font: {
-                weight: 'bold',
-                size: 14
+                weight: "bold",
+                size: 14,
               },
               formatter: (value, context) => {
-                const label = context.chart.data.labels?.[context.dataIndex] || '';
+                const label =
+                  context.chart.data.labels?.[context.dataIndex] || "";
                 return label;
               },
-              anchor: 'end', // Align the label outside the segment
-              align: 'end',
+              anchor: "end",
+              align: "end",
               offset: 10,
-            }
+            },
           },
           layout: {
             padding: {
@@ -127,25 +139,23 @@ const Impact = () => {
               bottom: 30,
               left: 90,
               right: 60,
-            }
+            },
           },
           elements: {
             arc: {
-              borderRadius: 10, // Rounded edges for the arcs
+              borderRadius: 10,
             },
           },
         },
       });
     }
 
-    // Cleanup function to destroy the chart on component unmount
     return () => {
       if (chartInstanceRef.current) {
         chartInstanceRef.current.destroy();
       }
     };
-  }, []); // Empty dependency array ensures the effect runs only once when the component mounts
-
+  }, []);
   return (
     <main className="bg-white">
       <section className="flex min-h-screen">
@@ -216,7 +226,6 @@ const Impact = () => {
         </div>
       </section>
 
-      {/* Nueva sección de Testimonios */}
       <section className="py-16">
         <div className="mx-auto max-w-6xl">
           <h2 className="mb-12 text-center text-4xl font-bold">
@@ -229,7 +238,7 @@ const Impact = () => {
 
             {[-1, 0, 1].map((offset) => {
               const testimonial = testimonials[getTestimonialIndex(offset)];
-              if (!testimonial) return null; // Safeguard to avoid undefined error
+              if (!testimonial) return null;
               return (
                 <div
                   key={testimonial.id}
@@ -237,16 +246,6 @@ const Impact = () => {
                     offset === 0 ? "w-1/2" : "w-1/4 opacity-60"
                   }`}
                 >
-                  <div className="mb-4 flex">
-                    {[1, 2, 3, 4, 5].map((star) => (
-                      <Star
-                        key={star}
-                        size={24}
-                        fill={star <= testimonial.rating ? "gold" : "none"}
-                        stroke={star <= testimonial.rating ? "gold" : "gray"}
-                      />
-                    ))}
-                  </div>
                   <p className="mb-4">
                     {offset === 0
                       ? testimonial.text
@@ -267,7 +266,6 @@ const Impact = () => {
       <section className="mx-auto max-w-6xl py-16">
         <h2 className="mb-12 text-center text-4xl font-bold">CASOS DE ÉXITO</h2>
 
-        {/* Contenedor de imágenes de ortodoncia */}
         <div className="mb-8 flex justify-center space-x-4">
           {[1, 2, 3, 4, 5, 6].map((index) => (
             <div key={index} className="relative">
@@ -280,9 +278,7 @@ const Impact = () => {
           ))}
         </div>
 
-        {/* Contenedor flex para texto y las imágenes de cirugías */}
         <div className="flex flex-col items-start justify-between md:flex-row">
-          {/* Texto descriptivo */}
           <div className="mb-8 max-w-2xl md:mb-0 md:pr-8">
             <p className="mb-4 text-sm">
               Lorem ipsum dolor sit amet consectetur. Rhoncus consequat est
@@ -300,7 +296,6 @@ const Impact = () => {
             </p>
           </div>
 
-          {/* Imágenes before/after a la derecha */}
           <div className="flex space-x-4">
             <img
               className="h-40 w-48 rounded-lg object-cover"
@@ -315,87 +310,91 @@ const Impact = () => {
           </div>
         </div>
       </section>
-      <section className="flex flex-col mt-20">
-          <div className="flex min-w-full">
-            <h2 className="text-[4vw] text-center sm:text-4xl lg:text-4xl w-[100%] text-black-600 font-bold mb-0">
-              ESTADÍSTICAS
-            </h2>
-          </div>
 
-          {/* Flex Container for Chart and Text */}
-          <div className="flex flex-row justify-center items-start w-full mt-10">
-            {/* Bar Chart Section with 70% Width */}
-            <div className="basis-[60%]">
-              <div className="px-4">
-                <div className="w-full px-20 py-10">
-                  <div>
-                    <div className="md:flex md:justify-between md:items-center">
-                      <div>
-                        <h2 className="text-xl text-gray-800 font-bold leading-tight">PACIENTES ATENDIDOS</h2>
-                        <p className="mb-2 text-gray-600 text-sm">CADA AÑO</p>
-                      </div>
+      <section className="mt-20 flex flex-col">
+        <div className="flex min-w-full">
+          <h2 className="text-black-600 mb-0 w-[100%] text-center text-[4vw] font-bold sm:text-4xl lg:text-4xl">
+            ESTADÍSTICAS
+          </h2>
+        </div>
+
+        <div className="mt-10 flex w-full flex-row items-start justify-center">
+          <div className="basis-[60%]">
+            <div className="px-4">
+              <div className="w-full px-20 py-10">
+                <div>
+                  <div className="md:flex md:items-center md:justify-between">
+                    <div>
+                      <h2 className="text-xl font-bold leading-tight text-gray-800">
+                        PACIENTES ATENDIDOS
+                      </h2>
+                      <p className="mb-2 text-sm text-gray-600">CADA AÑO</p>
                     </div>
+                  </div>
 
-                    <div className="my-8 relative">
-                      {/* Bar Chart */}
-                      <div className="flex items-end mb-2 space-x-2" style={{ height: `${fixedHeight}px` }}>
-                        {chartData.map((data, index) => (
-                          <div key={index} className="relative w-1/6">
-                            <div
-                              style={{ height: `${(data / maxValue) * fixedHeight}px` }} // Scale height based on max value
-                              className={`rounded-lg transition ease-in duration-200 ${
-                                data === maxValue ? 'bg-blue-600' : 'bg-blue-300'
-                              }`}
-                            >
-                              {/* Display the data value above the bar */}
-                              <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-1 text-gray-800 text-sm font-bold">
-                                {data}
-                              </div>
+                  <div className="relative my-8">
+                    <div
+                      className="mb-2 flex items-end space-x-2"
+                      style={{ height: `${fixedHeight}px` }}
+                    >
+                      {chartData.map((data, index) => (
+                        <div key={index} className="relative w-1/6">
+                          <div
+                            style={{
+                              height: `${(data / maxValue) * fixedHeight}px`,
+                            }}
+                            className={`rounded-lg transition duration-200 ease-in ${
+                              data === maxValue ? "bg-blue-600" : "bg-blue-300"
+                            }`}
+                          >
+                            <div className="absolute bottom-full left-1/2 mb-1 -translate-x-1/2 transform text-sm font-bold text-gray-800">
+                              {data}
                             </div>
                           </div>
-                        ))}
-                      </div>
+                        </div>
+                      ))}
+                    </div>
 
-                      {/* Labels */}
-                      <div className="border-t border-gray-400 mt-2"></div>
-                      <div className="flex items-end space-x-2 mt-2">
-                        {labels.map((label, index) => (
-                          <div key={index} className="relative w-1/6">
-                            <div className="text-center text-sm text-gray-700 font-bold">{label}</div>
+                    <div className="mt-2 border-t border-gray-400"></div>
+                    <div className="mt-2 flex items-end space-x-2">
+                      {labels.map((label, index) => (
+                        <div key={index} className="relative w-1/6">
+                          <div className="text-center text-sm font-bold text-gray-700">
+                            {label}
                           </div>
-                        ))}
-                      </div>
+                        </div>
+                      ))}
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-
-            {/* Text Section with 30% Width */}
-            <div className="basis-[40%] pr-10 py-20 flex felx-col justify-center items-center">
-            <p className="text-gray-800 text-base leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Curabitur bibendum ante ut feugiat semper. 
-                Integer interdum tincidunt nulla id ullamcorper. 
-                Aliquam erat volutpat. 
-                Phasellus vitae dui finibus, euismod ante at, tristique leo. 
-                Proin dictum justo at ante varius, 
-                facilisis euismod tellus mattis. 
-                Suspendisse rutrum ultricies consectetur. <br /><br />
-                Integer sagittis lectus eu ultricies vehicula.
-                Praesent turpis ante, tempus sit amet elit quis, porta placerat est. 
-              </p>
-            </div>
           </div>
 
-          {/* Doughnut Chart Section */}
-          <div className="flex w-full mx-auto ml-20 justify-start items-start">
-            <div className="w-[40%]">
-              <canvas ref={doughnutChartRef} id="myDoughnutChart"></canvas>
-            </div>
-            <h1 className="text-2xl font-bold text-left mt-20 ml-10 w-[60%]">PORCENTAJES DE PROCEDIMIENTOS <br /> REALIZADOS AL AÑO</h1>
+          <div className="felx-col flex basis-[40%] items-center justify-center py-20 pr-10">
+            <p className="text-base leading-relaxed text-gray-800">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              bibendum ante ut feugiat semper. Integer interdum tincidunt nulla
+              id ullamcorper. Aliquam erat volutpat. Phasellus vitae dui
+              finibus, euismod ante at, tristique leo. Proin dictum justo at
+              ante varius, facilisis euismod tellus mattis. Suspendisse rutrum
+              ultricies consectetur. <br />
+              <br />
+              Integer sagittis lectus eu ultricies vehicula. Praesent turpis
+              ante, tempus sit amet elit quis, porta placerat est.
+            </p>
           </div>
-        </section>
+        </div>
+
+        <div className="mx-auto ml-20 flex w-full items-start justify-start">
+          <div className="w-[40%]">
+            <canvas ref={doughnutChartRef} id="myDoughnutChart"></canvas>
+          </div>
+          <h1 className="ml-10 mt-20 w-[60%] text-left text-2xl font-bold">
+            PORCENTAJES DE PROCEDIMIENTOS <br /> REALIZADOS AL AÑO
+          </h1>
+        </div>
+      </section>
     </main>
   );
 };

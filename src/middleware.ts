4bd7f18356aa8c2eb57cf079ef4136b440aa2dba
SALV <a01707122@tec.mx>
2024-11-19 10:19:13 -0600

    [FIX] working middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { updateSession } from "./src/app/_components/library/sessionAuth";
+import { updateSession } from "./app/_components/library/sessionAuth";
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request);

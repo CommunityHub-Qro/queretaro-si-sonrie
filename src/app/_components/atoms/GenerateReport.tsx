a2d371d09a5bc447417105b8403810d89fa0dd34
rominanafu <romina.najera.fuentes@gmail.com>
2024-11-26 19:24:56 -0600

    [FIX] Report format

--- src/app/_components/atoms/GenerateReport.tsx
+++ src/app/_components/atoms/GenerateReport.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { getRecords } from "../hooks/getRecords";
 import moment from 'moment';
 
+const MONTHS = ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
+
 const GenerateReport: React.FC = () => {
       const records = getRecords(); // { data, error, isLoading }
       moment().format("MM/DD/YYYY");
@@ -12,35 +14,33 @@ const GenerateReport: React.FC = () => {
         const headers = ['', 'Pacientes atendidos'];
         const day = new Date();
 
-        // Unir los encabezados y los datos en formato CSV
         let rows = [[`Total`, `${records.data?records.data.length:0}`],
           [`Sexo:`],
           [`Hombres`, `NA`],
           [`Mujeres`, `NA`],
           [`Ingresos recientes:`],
-          [`este mes`, `${records.data?records.data.filter(
+          [`${MONTHS[day.getMonth()]} ${day.getFullYear()}`, `${records.data?records.data.filter(
               item => item.register_date.getMonth()==day.getMonth()
             ).length:0}`],
-          [`este anio`, `${records.data?records.data.filter(
+          [`${day.getFullYear()}`, `${records.data?records.data.filter(
               item => item.register_date.getFullYear()==day.getFullYear()
             ).length:0}`],
           [`Edades atendidas:`]
         ].join('\n');
 
         for (let i = 0; i < 18; i++) {
-          rows = [rows, [`${i} anios`, `${records.data?records.data.filter(
+          rows = [rows, [`${i}`, `${records.data?records.data.filter(
               item => moment().diff(item.birth_date, 'years') == i
             ).length:0}`]].join('\n');
         }
 
         try {
-          const response = await fetch('/api/Report'); // Llama al endpoint
+          const response = await fetch('/api/Report');
           if (!response.ok) {
             throw new Error('Error fetching treatment data');
           }
           const treatments = await response.json();
 
-          // Añade las filas de tratamientos al CSV
           rows = [rows, ['Tratamientos:', '']].join('\n');
           treatments.forEach((treatment: { title: string; count: number }) => {
             rows = [rows, [treatment.title, treatment.count.toString()]].join('\n');
@@ -49,13 +49,9 @@ const GenerateReport: React.FC = () => {
           console.error('Error fetching treatments:', error);
           rows = [rows, ['Error fetching treatments', '']].join('\n');
         }
-
-        // agregar cuantos pacientes hay por tratamiento/etapa
-        // **                                diagnostico
         
         const csvContent = [headers.join(','), rows].join('\n');
     
-        // Crear un enlace para descargar el archivo CSV
         const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
         const url = URL.createObjectURL(blob);
         const link = document.createElement('a');
@@ -63,7 +59,7 @@ const GenerateReport: React.FC = () => {
         link.setAttribute('download', 'data.csv');
         document.body.appendChild(link);
         link.click();
-        document.body.removeChild(link); // Elimina el enlace después de la descarga
+        document.body.removeChild(link);
       };    
 
   return (
